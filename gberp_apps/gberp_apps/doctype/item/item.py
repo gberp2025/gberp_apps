a6cf1ba65ea88919c8abf997ee664135cfc6543f
gberp2025 <erp@mail.gbforestry.com>
2025-02-26 12:26:45 +0000

    DocType Item - Update

---
+++ gberp_apps/gberp_apps/doctype/item/item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, GB and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Item(Document):
+	pass
